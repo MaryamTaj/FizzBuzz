47608886a975d92629415cd69672eb7816bdb03f
MaryamTaj <tajm48822@gmail.com>
2023-09-12 10:04:41 -0400

    Creating a main Java file

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        //FizzBuzz.main();
+        // Accessing a private method
+        FizzBuzz.doFizzBuzz(3);
+    }
 }
